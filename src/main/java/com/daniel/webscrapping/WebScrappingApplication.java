9c6495d34cb71483387de3e7d39081a92fe93016
daniel <daniel.cordeiro2@fatec.sp.gov.br>
2025-03-28 21:23:22 -0300

    :wrench: chore: adiciona setup

---
+++ src/main/java/com/daniel/webscrapping/WebScrappingApplication.java
@@ -0,0 +1,12 @@
+package com.daniel.webscrapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrappingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebScrappingApplication.class, args);
+  }
+}
